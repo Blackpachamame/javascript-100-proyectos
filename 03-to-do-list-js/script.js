03ee49a8d43c2e2a6afec471fad44daefb375ae7
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2024-03-21 14:57:29 -0300

    feat: Agregando input date

--- 03-to-do-list-js/script.js
+++ 03-to-do-list-js/script.js
@@ -3,29 +3,39 @@ import deleteIcon from "./components/deleteIcon.js";
 
 const btn = document.querySelector('[data-form-btn]');
 
+const addTask = (evento) => {
+  const list = document.querySelector('[data-list]');
+  const task = createTask(evento);
+  list.appendChild(task);
+};
+
 const createTask = (evento) => {
   evento.preventDefault();
   const input = document.querySelector('[data-form-input]');
+  const calendar = document.querySelector('[data-form-date]');
   const value = input.value;
-  const list = document.querySelector('[data-list]');
+  const date = calendar.value;
+  const dateFormat = moment(date).format('DD/MM/YYYY');
+  console.log(dateFormat);
   const task = document.createElement('li');
   task.classList.add('card');
   input.value = '';
-
-  // Backticks
+  //backticks
   const taskContent = document.createElement('div');
 
   const titleTask = document.createElement('span');
   titleTask.classList.add('task');
   titleTask.innerText = value;
-
   taskContent.appendChild(checkComplete());
   taskContent.appendChild(titleTask);
-
-  //task.innerHTML = content;
+  // task.innerHTML = content;
+  const dateElement = document.createElement('span');
+  dateElement.innerHTML = dateFormat;
   task.appendChild(taskContent);
+  task.appendChild(dateElement);
   task.appendChild(deleteIcon());
-  list.appendChild(task);
+  return task;
 };
 
-btn.addEventListener('click', createTask);
+//Arrow functions o funciones anonimas
+btn.addEventListener('click', addTask);
