e7472af35506173212d9a4402c5d736c0b7e9f5c
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2024-03-28 11:12:44 -0300

    feat: Eliminar datos del LocalStorage utilizando el método splice

--- 03-to-do-list-js/components/addTask.js
+++ 03-to-do-list-js/components/addTask.js
@@ -45,23 +45,19 @@ export const createTask = ({ value, dateFormat, complete, id }) => {
     const taskContent = document.createElement('div');
 
     const check = checkComplete(id);
+
     if (complete) {
         check.classList.toggle('fas');
         check.classList.toggle('completeIcon');
         check.classList.toggle('far');
     }
-
     const titleTask = document.createElement('span');
     titleTask.classList.add('task');
     titleTask.innerText = value;
     taskContent.appendChild(check);
     taskContent.appendChild(titleTask);
 
-    const dateElement = document.createElement('span');
-    dateElement.innerHTML = dateFormat;
-
     task.appendChild(taskContent);
-    // task.appendChild(dateElement);
-    task.appendChild(deleteIcon());
+    task.appendChild(deleteIcon(id));
     return task;
 };
